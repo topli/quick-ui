651f1fb2790937a3ec784c9073e4eb3e8599a62b
topli <tli@incarcloud.com>
2023-04-10 15:17:09 +0800

    优化table

--- src/components/Table/render-column.js
+++ src/components/Table/render-column.js
@@ -6,27 +6,10 @@ export default {
     options: Array
   },
   data() {
-    return {
-      val: {
-        value: '',
-        label: ''
-      }
-    }
-  },
-  created() {
+    return {}
   },
   methods: {
-    isChange() {
-      // columns key is empty return false
-      if (!this.prop) {
-        return false
-      }
-      const newValue = this.scope.row[this.prop]
-      const oldValue = this.val.value
-      return !(String(newValue) === String(oldValue))
-    },
     filterData() {
-      // this.val = {}
       if (this.options) {
         const val = this.scope.row[this.prop]
         const find = this.options.find(item => item.value === val) || {}
@@ -37,15 +20,11 @@ export default {
     }
   },
   render(h) {
-    let val = this.val
-    if (this.isChange()) {
-      // rerender
-      val = this.filterData()
-    }
+    const val = this.filterData()
     if (this.renderContent) {
       return this.renderContent(h, this.scope, val || {})
     } else {
-      return h('span', this.val.label)
+      return h('span', val.label)
     }
   }
 }
